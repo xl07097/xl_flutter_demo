d87b59c286cd777a17d871e808c721f619497b32
xueliang <xueliang@szusoft.com>
2020-06-09 15:25:10 +0800

    父组件管理state

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,11 @@ class MyApp extends StatelessWidget {
           return null;
         });
       },
-      initialRoute: "active",
+      initialRoute: "ParentWidget",
       routes: {
         "new_route": (context) => NewRoute(),
         "active": (context) => Tapboxa(),
+        "ParentWidget": (context) => ParentWidget(),
         "tip_route": (context) =>
             TipRoute(text: ModalRoute.of(context).settings.arguments),
         "/": (context) => MyHomePage(title: 'Flutter Demo Home Page'),
@@ -187,26 +188,69 @@ class _Tapboxa extends State<Tapboxa> {
     });
   }
 
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return new GestureDetector(
       onTap: _handle,
       child: new Container(
         child: new Center(
-          child: new Text(
-            _active ? 'Active' : 'Inactive',
-            style: new TextStyle(fontSize: 32.0, color:Colors.white),
-
-          )
-        ),
+            child: new Text(
+          _active ? 'Active' : 'Inactive',
+          style: new TextStyle(fontSize: 32.0, color: Colors.white),
+        )),
         width: 200.0,
         height: 200.0,
         decoration: new BoxDecoration(
-          color: _active ? Colors.lightBlue[700]:Colors.grey[600]
-        ),
+            color: _active ? Colors.lightBlue[700] : Colors.grey[600]),
       ),
     );
   }
 }
 
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => new _ParentWidgetState();
+}
 
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
 
+  void _handleTapboxState(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        child: new TapboxB(active: _active, onChanged: _handleTapboxState));
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+  final bool active;
+  final ValueChanged<bool> onChanged;
+  void _handle() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+        onTap: _handle,
+        child: new Container(
+            child: new Center(
+              child: new Text(
+                active ? 'Active' : 'Inactive',
+                style: new TextStyle(fontSize: 32.0, color: Colors.white),
+              ),),
+            width: 200.0,
+            height: 200.0,
+            decoration: new BoxDecoration(
+              color: active?Colors.lightGreen[300]:Colors.grey[600]
+            ),
+        )
+      );
+  }
+}
